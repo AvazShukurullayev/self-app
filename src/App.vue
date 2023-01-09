577cb7f2fe2fa7b4d0dce77741744b6e7488b2e4
Avaz <elonarm21@gmail.com>
2023-01-10 00:35:20 +0500

    files

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="content">
+        <h2>App info</h2>
+        <div class="search-panel">
+          <h2>Search Input</h2>
+          <h2>Filter buttons</h2>
+        </div>
+        <h2>Movie list</h2>
+        <h2>Movie add form</h2>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/ui-components/MyBox.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="box">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyBox",
+};
+</script>
+
+<style>
+.box {
+  margin: 1rem 0;
+  padding: 1rem;
+  box-shadow: 15px 15px 15px rgba(0, 0, 0, 0.15);
+  border-radius: 6px;
+  background-color: #f8f8f8c2;
+}
+</style>
